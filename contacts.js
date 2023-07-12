84bb1994ca7f3854bfce1760f9d5b5edab0ff02c
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-07-12 17:19:21 +0300

    add commander

--- contacts.js
+++ contacts.js
@@ -1,9 +1,63 @@
 const fs = require("fs/promises");
+const path = require("path");
 
-const contactsOperation = async () => {
-  const data = await fs.readFile("./db/contacts.json", "utf-8");
+const { nanoid } = require("nanoid");
 
-  console.log(data);
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+
+  return JSON.parse(contacts);
 };
 
-contactsOperation();
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+
+  const result = contacts.find((contact) => contact.id === contactId);
+
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const updateContactId = async (id, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactId,
+  removeContact,
+};
+
+// listContacts();
--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-console.log("Hello");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactId,
+  removeContact,
+} = require("./contacts.js");
+
+const { program } = require("commander");
+
+program
+  .option("--action <type>")
+  .option("--id <type>")
+  .option("--name <type>")
+  .option("--tweets <type>")
+  .option("--followers <type>")
+  .option("--avatar <type>");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+const incoveAction = async ({
+  action,
+  id,
+  name,
+  tweets,
+  followers,
+  avatar,
+}) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contact = await getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await addContact({ name, tweets, followers, avatar });
+      return console.log(newContact);
+
+    case "update":
+      const updateContact = await updateContactId(id, {
+        name,
+        tweets,
+        followers,
+        avatar,
+      });
+      return console.log(updateContact);
+
+    case "remove":
+      const removeCont = await removeContact(id);
+      return console.log(removeCont);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+incoveAction(argv);
+
+// incoveAction({ action: "list" });
+// incoveAction({ action: "get", id: "3" });
+
+// incoveAction({
+//   action: "add",
+//   name: "Djon Liscko",
+//   tweets: 305,
+//   followers: 100000,
+//   avatar: "./images/user13.png",
+// });
+
+// incoveAction({
+//   action: "update",
+//   name: "Djon",
+//   tweets: 5,
+//   followers: 1,
+//   avatar: "./images/user1.png",
+//   id: "1",
+// });
+
+// incoveAction({
+//   action: "remove",
+//   id: "4qVwSOOE5Pwx57UWguzzF",
+// });
